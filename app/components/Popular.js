7a4558ca4d3bdad694b523026a1314ffc267a555
Victor Rivera <verm2405@gmail.com>
2018-10-30 22:51:07 -0400

    Refactored Battle.js

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -28,28 +28,25 @@ function SelectLanguage(props) {
 function RepoGrid(props) {
   return (
     <ul className="popular-list">
-      {props.repos.map(
-        (repo, index) =>
-          console.log(repo) || (
-            <li key={repo.id} className="popular-item">
-              <div className="popular-rank">#{index + 1}</div>
-              <ul className="space-list-items">
-                <li>
-                  <img
-                    className="avatar"
-                    src={repo.owner.avatar_url}
-                    alt={"Avatar for " + repo.owner.login}
-                  />
-                </li>
-                <li>
-                  <a href={repo.html_url}>{repo.name}</a>
-                </li>
-                <li>@{repo.owner.login}</li>
-                <li>{repo.stargazers_count} stars</li>
-              </ul>
+      {props.repos.map((repo, index) => (
+        <li key={repo.id} className="popular-item">
+          <div className="popular-rank">#{index + 1}</div>
+          <ul className="space-list-items">
+            <li>
+              <img
+                className="avatar"
+                src={repo.owner.avatar_url}
+                alt={"Avatar for " + repo.owner.login}
+              />
             </li>
-          )
-      )}
+            <li>
+              <a href={repo.html_url}>{repo.name}</a>
+            </li>
+            <li>@{repo.owner.login}</li>
+            <li>{repo.stargazers_count} stars</li>
+          </ul>
+        </li>
+      ))}
     </ul>
   );
 }
