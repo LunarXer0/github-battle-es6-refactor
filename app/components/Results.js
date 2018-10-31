14f5f007fc97a8bd0c8844e737364950a03e8bdd
Victor Rivera <verm2405@gmail.com>
2018-10-31 01:17:12 -0400

    Converted Promises to Async/Await

--- app/components/Results.js
+++ app/components/Results.js
@@ -54,27 +54,24 @@ class Results extends React.Component {
     loading: true
   };
 
-  componentDidMount() {
+  async componentDidMount() {
     const { playerOneName, playerTwoName } = queryString.parse(
       this.props.location.search
     );
 
-    battle([playerOneName, playerTwoName]).then(players => {
-      if (players === null) {
-        return this.setState(() => ({
+    const players = await battle([playerOneName, playerTwoName]);
+    players === null
+      ? this.setState(() => ({
           error:
-            "Looks like there was an error. Check that both users exist on Github.",
+            "Looks like there was an error. Check that both users exist on GitHub.",
+          loading: false
+        }))
+      : this.setState(() => ({
+          error: null,
+          winner: players[0],
+          loser: players[1],
           loading: false
         }));
-      }
-
-      this.setState(() => ({
-        error: null,
-        winner: players[0],
-        loser: players[1],
-        loading: false
-      }));
-    });
   }
   render() {
     const { error, winner, loser, loading } = this.state;
